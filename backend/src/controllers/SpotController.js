e337c4e89575856240ec03f9abb1e1312be80650
Vagner Bernal de castro <vagner.bcastro@gmail.com>
2019-10-01 14:04:52 -0300

    Create back-end

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,40 @@
+const Spot = require("../models/Spot");
+const User = require("../models/User");
+
+module.exports = {
+  async index(req, res) {
+    const { tech } = req.query;
+
+    const spots = await Spot.find({ techs: tech });
+
+    return res.json(spots);
+  },
+
+  async store(req, res) {
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(400).json({ error: "User does not existe" });
+    }
+
+    let thumbnail;
+
+    if (req.file) {
+      thumbnail = req.file;
+    }
+
+    const { company, techs, price } = req.body;
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail,
+      company,
+      techs: techs.split(",").map(tech => tech.trim()),
+      price
+    });
+
+    return res.json(spot);
+  }
+};
